587021713b4306138f0644deea9e2acbba89e062
pinksquid <anthony.szymaniak@pinksquid>
2019-09-20 11:35:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# christmas trip website
+ christmas trip
---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+		sectionsColor: ['transparent', 'transparent', 'transparent'],
+		menu: '.main-nav ul',
+
+            easingcss3: 'cubic-bezier(0.645, 0.075, 0.33, 1)'
+	});
+});
